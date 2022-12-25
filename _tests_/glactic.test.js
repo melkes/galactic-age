39b1a3da82355b2affa59cf937f5ca0767f662cf
Mike Wilkes <mikewilkes@outlook.com>
2022-12-25 06:21:51 -0800

    2nd test received good fail

--- _tests_/glactic.test.js
+++ _tests_/glactic.test.js
@@ -6,4 +6,10 @@ describe('GalacticAge', () => {
   const galacticAge = new GalacticAge(65);
   expect(galacticAge.age).toEqual(65);
   });
+
+  test('should correctly calculate inputted age in Mars years', () => {
+    const galacticAge = new GalacticAge(65);
+    expect(galacticAge.marsYears).toEqual(15.6);
+  });
+
 });
