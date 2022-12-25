6b1cd0e347a41f7750a0dc941ef06fd7681cb3a5
Mike Wilkes <mikewilkes@outlook.com>
2022-12-25 03:55:03 -0800

    add ESLintPlugin to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintPlugin = require('eslint-webpack-plugin');   // new line!
 
 module.exports = {
   entry: './src/index.js',
