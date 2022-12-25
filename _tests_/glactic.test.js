31725371e48f9d78beaff9c05eefc44732e06095
Mike Wilkes <mikewilkes@outlook.com>
2022-12-25 06:00:38 -0800

    1st test received good fail

---
+++ _tests_/glactic.test.js
@@ -0,0 +1,9 @@
+import GalacticAge from "./../src/galactic.js";
+
+describe('GalacticAge', () => {
+
+  test('should create an object with an inputted age',() => {
+  const galacticAge = new GalacticAge(65);
+  expect(galacticAge.age).toEqual(65);
+  });
+});
