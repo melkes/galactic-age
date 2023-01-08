6c70df7948a317427788e19bf26759c97201a824
Mike Wilkes <mikewilkes@outlook.com>
2023-01-08 03:49:53 -0800

    4th test passing, green phase

--- src/galactic.js
+++ src/galactic.js
@@ -3,11 +3,17 @@ export default class GalacticAge {
     this.age = age;
   }
 
-  getMarsYears() {
-    return (this.age / 1.88).toFixed(1);
-  }
-
   getMercuryYears() {
     return (this.age / .24).toFixed(1);
   }
+  getVenusYears() {
+    return (this.age / .62).toFixed(1);
+  }
+  getMarsYears() {
+    return (this.age / 1.88).toFixed(1);
+  }
+  getJupiterYears() {
+    return (this.age / 11.86).toFixed(1);
+  }
+
 }
