9300ef79d6f4ad8552051bf18c744b7fa5cd8b94
Mike Wilkes <mikewilkes@outlook.com>
2023-01-08 06:10:40 -0800

    updated readme

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       verbose: true
     }), 
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Galactic Age Calculator',
       template: './src/index.html',
       inject: 'body'
     })
